280612eba518f87e5bbf0d9b8365b9307c7c74f3
rosesxmarie <140836653+rosesxmarie@users.noreply.github.com>
2026-09-18 17:35:33 -0600

    Added comments and implemented rook, bishop, and queeen

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -59,60 +59,174 @@ public class ChessPiece {
 
         ChessPiece piece = board.getPiece(myPosition);
         ChessGame.TeamColor myColor = piece.getTeamColor();
-        int row = myPosition.getRow();
-        int col = myPosition.getColumn();
 
         if (piece.getPieceType() == PieceType.KING) {
-            kingMoves(board, myPosition, row, col, myColor, moves);
+            kingMoves(board, myPosition, myColor, moves);
         }
 
         if (piece.getPieceType() == PieceType.KNIGHT) {
-            knightMoves(board, myPosition, row, col, myColor, moves);
+            knightMoves(board, myPosition, myColor, moves);
+        }
+
+        if (piece.getPieceType() == PieceType.ROOK) {
+            rookMoves(board, myPosition, myColor, moves);
+        }
+
+        if (piece.getPieceType() == PieceType.BISHOP) {
+            bishopMoves(board, myPosition, myColor, moves);
+        }
+
+        if (piece.getPieceType() == PieceType.QUEEN) {
+            queenMoves(board, myPosition, myColor, moves);
         }
 
         return moves;
     }
 
-    private void kingMoves (ChessBoard board, ChessPosition myPosition, int row, int col, ChessGame.TeamColor myColor, List<ChessMove> moves) {
+    private void kingMoves (ChessBoard board, ChessPosition myPosition, ChessGame.TeamColor myColor, List<ChessMove> moves) {
         int [][] directions = {
                 {1,0}, {-1,0}, {0,1}, {0,-1}, {1,1}, {-1,1}, {1,-1}, {-1,-1}
         };
 
+        // Try each possible directions
         for (int[] dir : directions) {
-            int targetRow = row + dir[0];
-            int targetCol = col + dir[1];
+            int targetRow = myPosition.getRow() + dir[0];
+            int targetCol = myPosition.getColumn() + dir[1];
 
+            // Check if in-bounds
             if (targetRow < 1 || targetRow > 8 || targetCol < 1 || targetCol > 8) {
                 continue;
             }
 
+            // Get the position and piece
             ChessPosition targetPosition = new ChessPosition(targetRow, targetCol);
-            ChessPiece newPiece = board.getPiece(targetPosition);
+            ChessPiece targetPiece = board.getPiece(targetPosition);
 
-            if (newPiece == null || newPiece.getTeamColor() != myColor) {
+            // Empty target or enemy, add move
+            if (targetPiece == null || targetPiece.getTeamColor() != myColor) {
                 moves.add(new ChessMove(myPosition, targetPosition, null));
             }
         }
     }
 
-    private void knightMoves (ChessBoard board, ChessPosition myPosition, int row, int col, ChessGame.TeamColor myColor, List<ChessMove> moves) {
+    private void knightMoves (ChessBoard board, ChessPosition myPosition, ChessGame.TeamColor myColor, List<ChessMove> moves) {
         int[][] directions = {
                 {2, 1}, {2, -1}, {-2, 1}, {-2, -1}, {1, 2}, {-1, 2}, {1, -2}, {-1, -2}
         };
 
+        // Try each possible directions
         for (int[] dir : directions) {
-            int targetRow = row + dir[0];
-            int targetCol = col + dir[1];
+            int targetRow = myPosition.getRow() + dir[0];
+            int targetCol = myPosition.getColumn() + dir[1];
 
+            // Check if in-bounds
             if (targetRow < 1 || targetRow > 8 || targetCol < 1 || targetCol > 8) {
                 continue;
             }
 
+            // Get the position and piece
             ChessPosition targetPosition = new ChessPosition(targetRow, targetCol);
-            ChessPiece newPiece = board.getPiece(targetPosition);
+            ChessPiece targetPiece = board.getPiece(targetPosition);
 
-            if (newPiece == null || newPiece.getTeamColor() != myColor) {
+            // Empty target or enemy, add move
+            if (targetPiece == null || targetPiece.getTeamColor() != myColor) {
                 moves.add(new ChessMove(myPosition, targetPosition, null));
+            }
+        }
+    }
+
+    private void rookMoves (ChessBoard board, ChessPosition myPosition, ChessGame.TeamColor myColor, List<ChessMove> moves) {
+        int[][] directions = {
+                {1, 0}, {-1, 0}, {0, 1}, {0, -1}
+        };
+
+        // Try each possible directions
+        for (int[] dir : directions) {
+            int targetRow = myPosition.getRow() + dir[0];
+            int targetCol = myPosition.getColumn() + dir[1];
+
+            // Check if in-bounds
+            while (targetRow >= 1 && targetRow <= 8 && targetCol >= 1 && targetCol <= 8) {
+                ChessPosition targetPosition = new ChessPosition(targetRow, targetCol);
+                ChessPiece targetPiece = board.getPiece(targetPosition);
+
+                if (targetPiece == null) {
+                    // Empty target? Keep moving
+                    moves.add(new ChessMove(myPosition, targetPosition, null));
+                } else {
+                    // Not empty? If it's an enemy, add move
+                    if (targetPiece.getTeamColor() != myColor) {
+                        moves.add(new ChessMove(myPosition, targetPosition, null));
+                    }
+                    // Empty or not, stop moving. This piece cannot jump over another piece
+                    break;
+                }
+                targetRow += dir[0];
+                targetCol += dir[1];
+            }
+        }
+    }
+
+    private void bishopMoves (ChessBoard board, ChessPosition myPosition, ChessGame.TeamColor myColor, List<ChessMove> moves) {
+        int[][] directions = {
+                {1, -1}, {1, 1}, {-1, 1}, {-1, -1}
+        };
+
+        // Try each possible directions
+        for (int[] dir : directions) {
+            int targetRow = myPosition.getRow() + dir[0];
+            int targetCol = myPosition.getColumn() + dir[1];
+
+            // Check if in-bounds
+            while (targetRow >= 1 && targetRow <= 8 && targetCol >= 1 && targetCol <= 8) {
+                ChessPosition targetPosition = new ChessPosition(targetRow, targetCol);
+                ChessPiece targetPiece = board.getPiece(targetPosition);
+
+                if (targetPiece == null) {
+                    // Empty target? Keep moving
+                    moves.add(new ChessMove(myPosition, targetPosition, null));
+                } else {
+                    // Not empty? If it's an enemy, add move
+                    if (targetPiece.getTeamColor() != myColor) {
+                        moves.add(new ChessMove(myPosition, targetPosition, null));
+                    }
+                    // Empty or not, stop moving. This piece cannot jump over another piece
+                    break;
+                }
+                targetRow += dir[0];
+                targetCol += dir[1];
+            }
+        }
+    }
+
+    private void queenMoves (ChessBoard board, ChessPosition myPosition, ChessGame.TeamColor myColor, List<ChessMove> moves) {
+        int[][] directions = {
+                {1, -1}, {1, 1}, {-1, 1}, {-1, -1}, {1, 0}, {-1, 0}, {0, 1}, {0, -1}
+        };
+
+        // Try each possible directions
+        for (int[] dir : directions) {
+            int targetRow = myPosition.getRow() + dir[0];
+            int targetCol = myPosition.getColumn() + dir[1];
+
+            // Check if in-bounds
+            while (targetRow >= 1 && targetRow <= 8 && targetCol >= 1 && targetCol <= 8) {
+                ChessPosition targetPosition = new ChessPosition(targetRow, targetCol);
+                ChessPiece targetPiece = board.getPiece(targetPosition);
+
+                if (targetPiece == null) {
+                    // Empty target? Keep moving
+                    moves.add(new ChessMove(myPosition, targetPosition, null));
+                } else {
+                    // Not empty? If it's an enemy, add move
+                    if (targetPiece.getTeamColor() != myColor) {
+                        moves.add(new ChessMove(myPosition, targetPosition, null));
+                    }
+                    // Empty or not, stop moving. This piece cannot jump over another piece
+                    break;
+                }
+                targetRow += dir[0];
+                targetCol += dir[1];
             }
         }
     }
